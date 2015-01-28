8157c3ec4cd17cf102bb2214aefd3d8df8b5cd0b
Mark S. Handcock <handcock@ucla.edu>
2015-01-28 18:19:01 +0000

    Fixed bug in td.quantile where it failed if the weights were not passed.
    git-svn-id: file:///Users/handcock/Build/svn/software/reldist@12 93528267-b521-4584-bb11-19111b5924ab

--- R/wtd.quantile.R
+++ R/wtd.quantile.R
@@ -16,37 +16,26 @@
 wtd.quantile <- function(x, q=0.5, na.rm = FALSE, weight=FALSE) {
  	if(mode(x) != "numeric")
  		stop("need numeric data")
+ 	if(missing(weight)|is.null(weight)){
+		return(quantile(x,probs=q,na.rm=na.rm))
+	}
+ 	if(length(weight)!=length(x)){
+		return(quantile(x,probs=q,na.rm=na.rm))
+	}
  	x <- as.vector(x)
  	wnas <- is.na(x)
  	if(sum(wnas)>0) {
- 		if(na.rm)
- 		 x <- x[!wnas]
- 	  	 if(!missing(weight)){weight <- weight[!wnas]}
- 		else return(NA)
+ 		if(na.rm){
+		 x <- x[!wnas]
+		 weight <- weight[!wnas]
+		}else{
+			return(NA)
+		}
  	}
- 	n <- length(x)
- 	half <- (n + 1)/2
- 	if(n %% 2 == 1) {
- 	  if(!missing(weight)){
- 		weight <- weight/sum(weight)
- 		sx <- sort.list(x)
- 		sweight <- cumsum(weight[sx])
- 		min(x[sx][sweight >= q])
- 	  }else{
- 		x[order(x)[half]]
- 	  }
- 	}
- 	else {
- 	  if(!missing(weight)){
- 		weight <- weight/sum(weight)
- 		sx <- sort.list(x)
- 		sweight <- cumsum(weight[sx])
- 		min(x[sx][sweight >= q])
- 	  }else{
- 		half <- floor(half) + 0:1
- 		sum(x[order(x)[half]])/2
- 	  }
- 	}
+ 	weight <- weight/sum(weight)
+ 	sx <- sort.list(x)
+ 	sweight <- cumsum(weight[sx])
+ 	min(x[sx][sweight >= q])
  }
 
 wtd.iqr <- function(x, na.rm = FALSE, weight=FALSE) {

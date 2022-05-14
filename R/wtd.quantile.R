e46b0f07e8d2649c199943cbadd9eb69b8fcdceb
handcock <handcock@ucla.edu>
2022-05-14 15:12:21 -0700

    Improved wtd.quantile code
    The wtd.quantile code introduced after 1.6-4 was in error and this version fixes it.
    
    Thanks to Domenico Suppa for pointing out the error.

--- R/wtd.quantile.R
+++ R/wtd.quantile.R
@@ -1,3 +1,4 @@
+#################################################################################
 # The <wtd.quantile> function determines and returns the weighted quantile of a
 # vector x
 #
@@ -11,16 +12,39 @@
 #   the weighted quantile    otherwise  
 #
 ################################################################################
-#' @importFrom Hmisc wtd.quantile
-wtd.quantile <- function(x, q=0.5, na.rm = FALSE, weight=FALSE) {
-	Hmisc::wtd.quantile(x=x,weights=weight,probs=q,na.rm=na.rm)
+
+wtd.quantile <- function(x, q=0.5, na.rm = FALSE, weight=NULL) {
+	if(mode(x) != "numeric")
+ 		stop("need numeric data")
+        if(!length(weight)){
+          quantile(x=x,probs=q,na.rm=na.rm)
+        }else{
+ 	  x <- as.vector(x)
+ 	  wnas <- is.na(x)
+ 	  if(sum(wnas)>0) {
+ 		if(na.rm){
+ 		 x <- x[!wnas]
+ 	  	 weight <- weight[!wnas]
+                }else{ return(NA) }
+ 	  }
+          o <- order(x)
+          n <- length(weight)
+          order <- 1 + (n - 1) * q
+          low  <- pmax(floor(order), 1)
+          high <- pmin(low + 1, n)
+          order <- order%%1
+          allq <- approx(x=cumsum(weight[o])/sum(weight), y=x[o], xout = c(low, high)/n, method = "constant", 
+              f = 1, rule = 2)$y
+          k <- length(q)
+          (1 - order) * allq[1:k] + order * allq[-(1:k)]
+        }
 }
 
-wtd.iqr <- function(x, na.rm = FALSE, weight=FALSE) {
+wtd.iqr <- function(x, na.rm = FALSE, weight=NULL) {
   wtd.quantile(x, q=0.75, na.rm = na.rm, weight=weight)
 - wtd.quantile(x, q=0.25, na.rm = na.rm, weight=weight)
 }
 iqr <- function(x, na.rm = FALSE) {
-  wtd.quantile(x, q=0.75, na.rm = na.rm, weight=FALSE)
-- wtd.quantile(x, q=0.25, na.rm = na.rm, weight=FALSE)
+  wtd.quantile(x, q=0.75, na.rm = na.rm, weight=NULL)
+- wtd.quantile(x, q=0.25, na.rm = na.rm, weight=NULL)
 }
